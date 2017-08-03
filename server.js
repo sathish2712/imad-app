f471482f8452bf13d66cb3faeb9a294430049c4e
sathish2712 <sathish27121998@gmail.com>
2017-08-03 19:22:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/artical one',function(req,res){
+app.get('/articalone',function(req,res){
     res.send('Artical one is servered here!!');
 }
 );
